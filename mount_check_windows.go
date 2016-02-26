fd2625bd802884e49b561b91f789fc6d364258f0
Russell Troxel <russell.troxel@rackspace.com>
2016-02-26 15:58:08 -0600

    added license

--- mount_check_windows.go
+++ mount_check_windows.go
@@ -1,7 +1,25 @@
+// Rackspace Cloud Monitoring plugin to check port, particularly
+// useful for services that aren't accessible to a remote port check.
+//
+// Copyright 2016 Russell Troxel <russell.troxel@rackspace.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/shirou/gopsutil/disk"
@@ -28,9 +46,9 @@ func test_rw(d string) int {
 
 	_, err := os.Create(p)
 	if err != nil {
-		return false
+		return 0
 	} else {
-		return true
+		return 1
 	}
 }
 
@@ -40,17 +58,24 @@ func main() {
 
 	drives := get_devices()
 
-	metric := "metric %s_writeable %s"
+	metric := "metric %s_writeable %d"
 	for i := 0; i < len(drives); i++ {
 		drive := drives[i]
 		rw := test_rw(drive)
-		if !(rw) {
+		if rw != 1 {
 			ro_devices += 1
-			append(ro_list, drive)
+			ro_list = append(ro_list, drive)
 		}
-		fmt.Sprintln(metric, drive, rw)
-
-		fmt.Sprintln("metric ro_devices %s", ro_devices)
-		fmt.Sprintln("metric ro_list %s", strings.Join(ro_list, ""))
+		fmt.Println(fmt.Sprintf(metric, drive, rw))
 	}
+
+	var list string
+	if len(ro_list) != 0 {
+		list = strings.Join(ro_list, "")
+	} else {
+		list = "none"
+	}
+
+	fmt.Println(fmt.Sprintf("metric ro_devices %d", ro_devices))
+	fmt.Println(fmt.Sprintf("metric ro_list %s", list))
 }
